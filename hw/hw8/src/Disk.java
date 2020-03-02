c5e2d3c18eaacbd59513460bec0381939d9cd3ba
Nathan Lee <leenathan72@gmail.com>
2020-03-01 22:27:24 -0800

    ...

--- hw/hw8/src/Disk.java
+++ hw/hw8/src/Disk.java
@@ -1,9 +1,9 @@
 import java.lang.StringBuffer;
 import java.util.concurrent;
 class Disk{
-	staic final int NUM_SECTORS = 1024;
+	static final int NUM_SECTORS = 1024;
 	Semaphore semaphore = Semaphore(1,True);
-	StringBuffer sectors[] = new StringBuffer[NUM_SECTORS]
+	StringBuffer sectors[] = new StringBuffer[NUM_SECTORS];
 	void write(int sector, StringBuffer data);
 	void read(int sector, StringBuffer data);
 }
