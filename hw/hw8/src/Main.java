7da53e8c1f517e984b132b3ed2730662ef92a4b7
Nathan Lee <leenathan72@gmail.com>
2020-03-02 23:34:00 -0800

    ...

--- hw/hw8/src/Main.java
+++ hw/hw8/src/Main.java
@@ -8,6 +8,7 @@ public class Main{
 		int NumDisks = Character.getNumericValue(args[4].charAt(1));
 		int NumPrinters = Character.getNumericValue(args[5].charAt(1));
 		ArrayList UserList = new ArrayList();
+		System.out.println("hello");
 		for (int i = 1; i < 1 + NumUsers; i++)
 		{
 			String file = "/home/namhoonl/cs141/hw/hw8/inputs/" + args[i];
