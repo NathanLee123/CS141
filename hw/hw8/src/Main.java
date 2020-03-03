e8b611f5d4f1b3c8dfd4283943c4b84e3f5c5344
Nathan Lee <leenathan72@gmail.com>
2020-03-02 22:09:40 -0800

    ...

--- hw/hw8/src/Main.java
+++ hw/hw8/src/Main.java
@@ -6,10 +6,11 @@ public class Main{
 		int NumUsers = Character.getNumericValue(args[0].charAt(1));
 		int NumDisks = Character.getNumericValue(args[4].charAt(1));
 		int NumPrinters = Character.getNumericValue(args[5].charAt(1));
-
+		List UserList = new ArrayList()
 		for (int i = 1; i < 1 + NumUsers; i++)
 		{
-			System.out.println(args[i]);
+			UserThread user = new UserThread(args[i]);
+			user.start()
 		}
 	}
 }
--- hw/hw8/src/UserThread.java
+++ hw/hw8/src/UserThread.java
@@ -1,7 +1,18 @@
 package src;
 import java.lang.StringBuffer;
 import java.lang.Thread;
+import java.io.BufferedReader;
 class UserThread{
 	StringBuffer CurrentLine = new StringBuffer();
-	
+	String user;
+	BufferedReader buffer;
+	UserThread(String newUser){
+		user = newUser;
+		buffer = new BufferedReader(new FileReader("hw8/resources/" + newUser));
+	}
+
+	void start(){
+		System.out.println(buffer.readLine())
+	}
+
 }
