64f437e459e159a4a66eaa19a20ec52c2302cc28
Nathan Lee <leenathan72@gmail.com>
2020-03-02 18:42:43 -0800

    ...

---
+++ hw/hw8/src/Main.java
@@ -0,0 +1,5 @@
+package src;
+
+public static void main(String[] args){
+	UserThread x = new UserThread();
+}
--- hw/hw8/src/UserThread.java
+++ hw/hw8/src/UserThread.java
@@ -3,4 +3,5 @@ import java.lang.StringBuffer;
 import java.lang.Thread;
 class UserThread{
 	StringBuffer CurrentLine = new StringBuffer();
+	
 }
