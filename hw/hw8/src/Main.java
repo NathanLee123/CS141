01558cf454c1797e3c183268dbe4470f0612a198
Nathan Lee <leenathan72@gmail.com>
2020-03-02 19:11:16 -0800

    ...

--- hw/hw8/src/Main.java
+++ hw/hw8/src/Main.java
@@ -3,9 +3,9 @@ package src;
 public class Main{
 
 	public static void main(String[] args){
-		int NumUsers = Integer.parseInt(args[0].charAt(1));
-		int NumDisks = Integer.parseInt(args[4].charAt(1));
-		int NumPrinters = Integer.parseInt(args[5].charAt(1));
+		int NumUsers = Character.getNumericValue(args[0].charAt(1));
+		int NumDisks = Character.getNumericValue(args[4].charAt(1));
+		int NumPrinters = Character.getNumericValue(args[5].charAt(1));
 
 		for (int i = 1; i < 1 + NumUsers; i++)
 		{
