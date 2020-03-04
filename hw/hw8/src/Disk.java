697d72691722b334e6328687ec5513709937d7c3
Nathan Lee <leenathan72@gmail.com>
2020-03-04 02:46:41 -0800

    ...

--- hw/hw8/src/Disk.java
+++ hw/hw8/src/Disk.java
@@ -7,16 +7,7 @@ class Disk{
 	StringBuffer sectors[] = new StringBuffer[NUM_SECTORS];
 	int freeSector = 0;
 	void write(int sector, StringBuffer data){
-		for(int i = 0; i < data.length(); i++){
-			sectors[sector].setCharAt(i,data.charAt(i));
-			freeSector++;
-		}
-		try{
-			Thread.sleep(200);
-		}
-		catch(InterruptedException e){
-			System.out.println("InterruptedException");
-		}
+		System.out.println(data.toString());
 	}
 	StringBuffer read(int sector){
 		return sectors[sector];
