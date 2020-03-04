be40bdda5a11645a1ac4e4753ede8ce174373e17
Nathan Lee <leenathan72@gmail.com>
2020-03-04 02:43:37 -0800

    ...

--- hw/hw8/src/Printer.java
+++ hw/hw8/src/Printer.java
@@ -6,16 +6,19 @@ class Printer {
 		this.id = id;
 	}
 	void print(StringBuffer data){
-		BufferedWriter out = new BufferedWriter(new FileWriter("PRINTER"+String.valueOf(id)));
-		out.write(data.toString(),0,data.length());
-		out.newLine();
-		out.flush();
-		out.close();
 		try{
+			BufferedWriter out = new BufferedWriter(new FileWriter("PRINTER"+String.valueOf(id)));
+			out.write(data.toString(),0,data.length());
+			out.newLine();
+			out.flush();
+			out.close();
 			Thread.sleep(2750);
 		}
 		catch(InterruptedException e){
 			System.out.println("InterruptedException");
+		}
+		catch(IOException e){
+			System.out.println("IOException");
 		}
 	}  // call sleep
 }
