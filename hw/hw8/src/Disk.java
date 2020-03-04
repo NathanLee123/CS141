f14d37475a5e624fea680d1e891d83f1a7abaad4
Nathan Lee <leenathan72@gmail.com>
2020-03-04 12:29:41 -0800

    ...

--- hw/hw8/src/Disk.java
+++ hw/hw8/src/Disk.java
@@ -8,7 +8,7 @@ class Disk{
 	int freeSector = 0;
 	void write(int sector, StringBuffer data){
 		for(int i = 0 ; i < data.length(); i++){
-			sectors[sector].setCharAt(i, data.CharAt(i));
+			sectors[sector].setCharAt(i, data.charAt(i));
 		}
 	}
 	StringBuffer read(int sector){
