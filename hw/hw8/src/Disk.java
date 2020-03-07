eef415a5bf9039a2b9e05597a9d9a80831302fe1
Nathan Lee <leenathan72@gmail.com>
2020-03-07 15:37:29 -0800

    3/7

--- hw/hw8/src/Disk.java
+++ hw/hw8/src/Disk.java
@@ -7,13 +7,13 @@ class Disk{
 	StringBuffer sectors[] = new StringBuffer[NUM_SECTORS];
 	int freeSector = 0;
 	void write(int sector, StringBuffer data){
-		System.out.println("Writing to sector" + Integer.toString(sector) +": " + data.toString());
+		//System.out.println("Writing to sector" + Integer.toString(sector) +": " + data.toString());
 		for(int i = 0 ; i < data.length(); i++){
 			sectors[sector] = new StringBuffer(data.toString());
 		}
 	}
 	StringBuffer read(int sector){
-		System.out.println("Reading data: "+ sectors[sector].toString());
+		//System.out.println("Reading data: "+ sectors[sector].toString());
 		return sectors[sector];
 	}
 }
